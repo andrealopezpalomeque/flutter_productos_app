c84b4be6812c625b490bb8ab25c0b1af50ac1311
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2023-12-01 11:13:39 -0300

    diseño cards productos

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -11,7 +11,67 @@ class ProductCard extends StatelessWidget {
         margin: const EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 420,
+        decoration: _cardBorders(),
+        child: const Stack(
+          alignment: Alignment.bottomLeft,
+          children: [_BackgroundImage(), _ProductDetails()],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(25),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black12,
+          blurRadius: 10, // soften the shadow
+          offset: Offset(0, 5), // move the shadow (x,y)
+        ),
+      ],
+    );
+  }
+}
+
+class _ProductDetails extends StatelessWidget {
+  const _ProductDetails({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        height: 70,
         color: Colors.red,
+        child: const Column(
+          children: [
+            Text('Disco Duro',style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+          ],
+        ));
+  }
+}
+
+class _BackgroundImage extends StatelessWidget {
+  const _BackgroundImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity, // to fill the parent
+        height: 420,
+        color: Colors.red,
+        child: const FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
