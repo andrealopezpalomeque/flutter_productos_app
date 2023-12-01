d5c4ef0290e7dc7517d8477e356ab3c6f7bb60f0
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2023-12-01 11:41:58 -0300

    diseño descripcion del producto

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -42,16 +42,42 @@ class _ProductDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: double.infinity,
-        height: 70,
-        color: Colors.red,
-        child: const Column(
-          children: [
-            Text('Disco Duro',style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
-          ],
-        ));
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          width: double.infinity,
+          height: 70,
+          decoration: _buildBoxDecoration(),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Disco Duro',
+                style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                
+                ),
+                Text(
+                'Id del disco duro',
+                style: TextStyle(fontSize: 15, color: Colors.white),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                
+                ),
+    
+            ],
+          )),
+    );
   }
+
+  BoxDecoration _buildBoxDecoration() => const BoxDecoration(
+      color: Colors.indigo,
+      borderRadius:  BorderRadius.only(
+          bottomLeft: Radius.circular(25), topRight: Radius.circular(25))
+
+  );
 }
 
 class _BackgroundImage extends StatelessWidget {
