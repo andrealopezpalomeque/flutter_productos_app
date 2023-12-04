8d646ee5d679d43f3972c267c1023db0b9f1774c
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2023-12-04 12:44:27 -0300

    guardo el token

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,14 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
   final String _baseUrl = 'identitytoolkit.googleapis.com';
   final String _firabaseToken = 'AIzaSyCUWmF1lZm519IxP49TplKjTf2dKy4X-DE';
+
+  final storage = new FlutterSecureStorage();
+
   // ! ---------CREAR USUARIO----------------
   Future<String?> createUser(String email, String password) async {
     final Map<String, dynamic> authData = {
@@ -21,6 +25,7 @@ class AuthService extends ChangeNotifier {
     final Map<String, dynamic> decodedResp = json.decode(resp.body);
 
     if (decodedResp.containsKey('idToken')) {
+      await storage.write(key: 'token', value: decodedResp['idToken']);
       return null;
     } else {
       return decodedResp['error']['message'];
@@ -43,9 +48,17 @@ class AuthService extends ChangeNotifier {
     final Map<String, dynamic> decodedResp = json.decode(resp.body);
 
     if (decodedResp.containsKey('idToken')) {
+      await storage.write(
+          key: 'token', value: decodedResp['idToken']); //! guarda el token
       return null;
     } else {
       return decodedResp['error']['message'];
     }
+  }
+
+// ! ---------LOGOUT----------------
+  Future logout() async {
+    await storage.delete(key: 'token'); //! borra el token
+    return;
   }
 }
